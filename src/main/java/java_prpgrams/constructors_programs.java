d5364d82e334eff24b84e5a141c5cd1b5706390a
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2022-10-31 20:40:58 +0530

    first commit

---
+++ src/main/java/java_prpgrams/constructors_programs.java
@@ -0,0 +1,20 @@
+package java_prpgrams;
+
+
+
+class First
+{
+	public First(int a, int b)
+	{
+		System.out.println("addition "+(a+b));
+	}
+}
+public class constructors_programs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		First obj = new First(10, 20);
+
+	}
+
+}
