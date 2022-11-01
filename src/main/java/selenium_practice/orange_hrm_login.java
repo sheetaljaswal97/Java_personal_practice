3a01e23ed9a335b23b91613149b60c124bd8b026
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2022-11-01 22:11:08 +0530

    second commit

---
+++ src/main/java/selenium_practice/orange_hrm_login.java
@@ -0,0 +1,32 @@
+package selenium_practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class orange_hrm_login {
+
+	@Test
+	@Parameters({"username","password"})
+	public void login(String username,String password) throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(3000);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys(username);
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(10000);
+		driver.quit();
+		
+	}
+
+}
